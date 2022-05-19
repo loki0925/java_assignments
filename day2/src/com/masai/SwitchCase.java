87866c5664c84e0c46798e668bc23bae2370bbc0
loki0925 <shreyashahuja@gmail.com>
2022-05-19 10:40:02 +0530

    Create SwitchCase.java

---
+++ day2/src/com/masai/SwitchCase.java
@@ -0,0 +1,29 @@
+package com.masai;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String city = "Mumbai"; 
+		
+		switch(city) {
+		case("Mumbai"):
+			System.out.println("Financial city");
+			break;
+		case("Kolkata"):
+			System.out.println("City of Joy");
+			break;
+		case("Delhi"):
+			System.out.println("Capital of the country");
+			break;
+		case("Bangalore"):
+			System.out.println("Cyber City");
+			break;
+		default:
+			System.out.println("May be Other Indian City");
+		
+
+	}
+
+}
+}
