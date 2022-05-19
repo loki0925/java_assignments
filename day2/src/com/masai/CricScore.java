e6ef70466069c122326dad71ed3cc69aa2ab2939
loki0925 <shreyashahuja@gmail.com>
2022-05-19 10:40:15 +0530

    Create CricScore.java

---
+++ day2/src/com/masai/CricScore.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+public class CricScore {
+	public static void main(String[] args) {
+		
+		int One = 1;
+        int two = 1;
+        int three = 1;
+        int four = 1;
+        int six = 1;
+        System.out.println("Total runs scored by Batsman are " + ((1*One)+(2*two)+(3*three)+(4*four)+(6*six)));
+	}
+
+}
