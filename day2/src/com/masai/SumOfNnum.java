975253f9baf189135c08e3b77b341fc8f52294d0
loki0925 <shreyashahuja@gmail.com>
2022-05-19 10:40:06 +0530

    Create SumOfNnum.java

---
+++ day2/src/com/masai/SumOfNnum.java
@@ -0,0 +1,16 @@
+package com.masai;
+
+public class SumOfNnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10; 
+		int sum = 0;
+    	for(int i=1;i<=n;i++) {
+    		sum += i;
+    	}
+    	System.out.println(sum);
+		
+	}
+
+}
