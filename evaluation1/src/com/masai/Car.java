9c296da54bb3978663bc17da31c915b7ad127bb9
loki0925 <shreyashahuja@gmail.com>
2022-05-23 15:44:20 +0530

    Create Car.java
    hello

---
+++ evaluation1/src/com/masai/Car.java
@@ -0,0 +1,16 @@
+package com.masai;
+
+public class Car {
+	
+	String model;
+
+	String companyName;
+	String Color;
+	Engine engine;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
