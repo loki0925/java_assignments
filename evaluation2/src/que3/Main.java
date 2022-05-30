5a29654810101ab88e5032797fa4e94dd7570619
loki0925 <shreyashahuja@gmail.com>
2022-05-30 15:49:05 +0530

    Update Main.java

--- evaluation2/src/que3/Main.java
+++ evaluation2/src/que3/Main.java
@@ -1,5 +1,19 @@
 package que3;
-
+ import java.util.*;
 public class Main {
+	
+	
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		//Write logic to get numberOfPassenger and numberOfKms
+
+		Ola myOla = new Ola();
+		Car myCar = myOla.bookCar(numberOfPassenger, numberOfKms);
+		int res = myOla.calculateBill(myCar);
+
+		System.out.println("The total fare amount is"+ res);
+	}
 
 }
