ff20ad0a3cc3279c0187eba489d60d2af48b8597
loki0925 <shreyashahuja@gmail.com>
2022-05-30 16:03:03 +0530

    Update Person.java

--- evaluation2/src/que4/Person.java
+++ evaluation2/src/que4/Person.java
@@ -4,5 +4,17 @@ public class Person {
  
 	String name;
 	String gender;
+	 Address address;
 	
+	
+	public Person(String name, String gender, Address address) {
+		super();
+		this.name = name;
+		this.gender = gender;
+		this.address = address;
+	}
+	
+	public Person() {
+		super();
+	}
 }
