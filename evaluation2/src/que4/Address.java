2ae17e8f6f3542204993b47199e6638496a360da
loki0925 <shreyashahuja@gmail.com>
2022-05-30 16:03:18 +0530

    Update Address.java

--- evaluation2/src/que4/Address.java
+++ evaluation2/src/que4/Address.java
@@ -4,6 +4,22 @@ public class Address {
  
 	String city;
 	String state;
-	String  pincode;
+	String  pinCode;
+	
+	public Address(String city, String state, String pinCode) {
+		super();
+		this.city = city;
+		this.state = state;
+		this.pinCode = pinCode;
+	}
+	
+	public Address(){
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Address [city=" + city + ", state=" + state + ", pinCode=" + pinCode + "]";
+	}
 	
 }
