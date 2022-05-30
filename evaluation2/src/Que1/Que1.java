f64bb6792f4b858fcf0357b97a4b7176e97499d4
loki0925 <shreyashahuja@gmail.com>
2022-05-30 15:46:54 +0530

    Update Que1.java

--- evaluation2/src/Que1/Que1.java
+++ evaluation2/src/Que1/Que1.java
@@ -1,5 +1,30 @@
 package Que1;
 
 public class Que1 {
-
+	//is-A relation is achieved through inheritance while Has-a is not achived through inheritance
+	//bike has engine is a example of Has-a while spendor is a bike is a example of is-a 
+	//
+	class bike{
+		String iambilke;
+	}
+	class spendor extends bike{
+		String iam_child_of_bike;
+		//here we can inherit the property of parent 
+	}
+	class engine{
+		String i_am_part_of_spendor;
+		//here we can create object of spendor and create a Has-a relation ship 
+	}
+	
+	//2) accessmodifiers 
+	
+	//Public: we can access public modifires everywhere trouth quliafied name from any package
+	//
+	
+	//default: in a perticuler package we can access default modifier 
+	
+   //protected : to access this kind of modifier inheritance is must and you can access only in a periculer package
+	
+	
+	//private: in can be access only in perticuler class outside class you cant acess it
 }
