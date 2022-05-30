b400d201933dac1d20e9c3c8e57c35d63285d1ba
loki0925 <shreyashahuja@gmail.com>
2022-05-30 15:48:33 +0530

    Update Main.java

--- evaluation2/src/que4/Main.java
+++ evaluation2/src/que4/Main.java
@@ -2,4 +2,17 @@ package que4;
 
 public class Main {
 
+	
+	public static Person generatePerson(Person person) {
+		
+	}
+	public static void main(String[] args) {
+
+		Person newStudent = generatePerson(new Student());
+
+		Person newTeacher = generatePerson(new Teacher());
+
+		System.out.println(newStudent);
+		System.out.println(newTeacher)
+		}
 }
