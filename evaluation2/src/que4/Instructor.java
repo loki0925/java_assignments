585488b7763ef383cf8a7fab1af439ed6898fc36
loki0925 <shreyashahuja@gmail.com>
2022-05-30 16:03:13 +0530

    Update Instructor.java

--- evaluation2/src/que4/Instructor.java
+++ evaluation2/src/que4/Instructor.java
@@ -2,6 +2,24 @@ package que4;
 
 public class Instructor extends Person{
 
-	int instructorid;
+	int instructorId;
 	int salary;
+	
+	public Instructor(String name, String gender, Address address, int instructorId, int salary) {
+		super(name, gender, address);
+		this.instructorId = instructorId;
+		this.salary = salary;
+	}
+
+	public Instructor() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Instructor [instructorId=" + instructorId + ", salary=" + salary +", address="+
+	this.address+ "]";
+	}
+	
+	
 }
