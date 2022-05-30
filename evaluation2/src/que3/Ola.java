5ba84f8bea6ede58b556b2be99ec31cba741027e
loki0925 <shreyashahuja@gmail.com>
2022-05-30 15:48:53 +0530

    Update Ola.java

--- evaluation2/src/que3/Ola.java
+++ evaluation2/src/que3/Ola.java
@@ -2,4 +2,21 @@ package que3;
 
 public class Ola {
 
+	public Car bookCar(int numberOfPassenger, int numberOfKMs)
+	{
+		if(numberOfPassenger >=3) {
+			Car a1 = new HatchBack();
+			return a1;
+		}
+		else {
+			Car a2 = new Sedan();
+			return a2;
+		}
+	}
+
+	public int calculateBill(Car car) {
+		Car c1 = new Car();
+		
+		Total fare=numberOfKms*farePerKm;
+	}
 }
