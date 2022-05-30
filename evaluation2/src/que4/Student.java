2e671d8f5d3d9cccdcfe68a1f3f509455483d008
loki0925 <shreyashahuja@gmail.com>
2022-05-30 16:02:57 +0530

    Update Student.java

--- evaluation2/src/que4/Student.java
+++ evaluation2/src/que4/Student.java
@@ -2,9 +2,27 @@ package que4;
 
 public class Student extends Person {
 
-	int studentid;
-	int courseEnrolled;
+	int studentId;
+	String courseEnrolled;
 	int courseFee;
+	
+	
+	public Student(String name, String gender, Address address, int studentId, String courseEnrolled, int courseFeel) {
+		super(name, gender, address);
+		this.studentId = studentId;
+		this.courseEnrolled = courseEnrolled;
+		this.courseFee = courseFeel;
+	}
+	
+	public Student() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Student [StudentId="+this.studentId+", CourseFees = "+this.courseFee+" courseEnrolled = "
+				+ ""+this.courseEnrolled+"Address = "+this.address;
+	}
 	
 	
 }
